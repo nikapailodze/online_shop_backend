33076d0a1e7afb3c0f7ee53b958cb3ca8aea7a27
nikapailodze <nikapail.a01@gmail.com>
2025-12-08 17:26:57 -0500

    fix:Added a MySQL-specific migration to force AUTO_INCREMENT on all primary keys   (Users, Products, CartItems, Orders, OrderItems). Path: OnlineShopBackend/   Migrations/20251209113000_MakeIdsAutoIncrement_MySql.cs.

---
+++ Migrations/20251209113000_MakeIdsAutoIncrement_MySql.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShopBackend.Migrations
+{
+    public partial class MakeIdsAutoIncrement_MySql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider.Contains("MySql", StringComparison.OrdinalIgnoreCase))
+            {
+                migrationBuilder.Sql("ALTER TABLE `Users` MODIFY COLUMN `Id` INT NOT NULL AUTO_INCREMENT;");
+                migrationBuilder.Sql("ALTER TABLE `Products` MODIFY COLUMN `Id` INT NOT NULL AUTO_INCREMENT;");
+                migrationBuilder.Sql("ALTER TABLE `CartItems` MODIFY COLUMN `Id` INT NOT NULL AUTO_INCREMENT;");
+                migrationBuilder.Sql("ALTER TABLE `Orders` MODIFY COLUMN `Id` INT NOT NULL AUTO_INCREMENT;");
+                migrationBuilder.Sql("ALTER TABLE `OrderItems` MODIFY COLUMN `Id` INT NOT NULL AUTO_INCREMENT;");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider.Contains("MySql", StringComparison.OrdinalIgnoreCase))
+            {
+                migrationBuilder.Sql("ALTER TABLE `OrderItems` MODIFY COLUMN `Id` INT NOT NULL;");
+                migrationBuilder.Sql("ALTER TABLE `Orders` MODIFY COLUMN `Id` INT NOT NULL;");
+                migrationBuilder.Sql("ALTER TABLE `CartItems` MODIFY COLUMN `Id` INT NOT NULL;");
+                migrationBuilder.Sql("ALTER TABLE `Products` MODIFY COLUMN `Id` INT NOT NULL;");
+                migrationBuilder.Sql("ALTER TABLE `Users` MODIFY COLUMN `Id` INT NOT NULL;");
+            }
+        }
+    }
+}
